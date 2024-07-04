055c8ddf0374f39008a2c2c32201d1468f8fe865
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-07-04 15:41:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,56 +1,34 @@
-const CACHE_NAME = 'Cache-v1';
-const OFFLINE_PAGE = "/offline.html";
-// Guardo en el cache los archivos de la aplicación
+const CACHE_NAME = 'cache-v1';
 const CACHE_FILES = [
-    '/',
-    'index.html',
-    'style.css',
-    'favoritos.html',
-    'offline.html',
-    'historial.html',
-    'app.js',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-    'img/icon.png',
-    'img/fondo.jpg',
-    'img/offline.png',
-    'img/foto.png'
+  '/',
+  'index.html',
+  'favoritos.html',
+  'estilos.css',
+  'app.js',
+  'favoritos.js',
+  'libs/pouchdb-8.0.1.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11',
+  'img/icono-cine.png',
+  'img/icono-error.png'
 ];
 
-// Cuando se instala el SW, Guardo todo en el cache
-self.addEventListener('install', (e) => {
-    const cache = caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(CACHE_FILES);
-    });
-    // Espero hasta que la promesa termine
-    e.waitUntil(cache);
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('CACHE OPENED');
+      return cache.addAll(CACHE_FILES);
+    }).catch(function(error) {
+      console.error('Error al intentar agregar recursos al caché:', error);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    // Primero Desde la Web
-    const respuesta = fetch(e.request).then(respNet => {
-        // Aprovecho y guardo en el cache lo nuevo
-        return caches.open(CACHE_NAME).then(cache => {
-            cache.put(e.request, respNet.clone());
-            return respNet;
-        });
-    // Si no hay conexión
-    }).catch(error => {
-        return caches.match(e.request);
-    });
-
-    e.respondWith(respuesta);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
-
-function fromCache(request) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            if (!matching || matching.status === 404) {
-                if (request.destination !== "document" || request.mode !== "navigate") {
-                    return Promise.reject("no-match");
-                }
-                return cache.match(OFFLINE_PAGE);
-            }
-            return matching;
-        });
-    });
-}
